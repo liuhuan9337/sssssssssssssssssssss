f9b0fcd71531ff6cec1af6e7ea5bedf7f6c4e866
刘欢 <18231416039@163.com>
2016-03-24 14:10:22 +0800

    第一次

---
+++ sssssssssssssssssssssss/sssssssssssssssssssssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sssssssssssssssssssssss
+//
+//  Created by 刘欢 on 16/3/24.
+//  Copyright © 2016年 刘欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sssssssssssssssssssssss/sssssssssssssssssssssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sssssssssssssssssssssss
+//
+//  Created by 刘欢 on 16/3/24.
+//  Copyright © 2016年 刘欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
